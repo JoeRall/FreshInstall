7797b4ec71b5ab37e9b698f1da925b17b7bc28a3
Joe <joseph.rall@live.com>
2015-03-29 13:04:26 -0400

    Deleted some files I won't be needing.

--- tests.ps1
+++
@@ -1,6 +0,0 @@
-$defaultComputerName = "Acer-S7"
-$computerName = Read-Host -Prompt "Please Enter the computer name [$defaultComputerName]"
-if ($computerName -eq '') { $computerName = $defaultComputerName}
-
-Write-Output "Setting computer name to $computerName"
-Rename-Computer $computerName
