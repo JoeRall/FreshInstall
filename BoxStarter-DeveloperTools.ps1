cd6a296365b259761e4ef237c844ec4fd4917080
Joe Rall <Joseph.Rall@Live.com>
2015-03-31 12:57:30 -0400

    Added NUnitTestAdapter to DeveloperTools

--- BoxStarter-DeveloperTools.ps1
+++ BoxStarter-DeveloperTools.ps1
@@ -14,6 +14,7 @@ if ($installVS -eq '') { cinst -y VisualStudio2013Ultimate Blend SQL Win8SDK Win
 #VS Extensions
 Install-ChocolateyVsixPackage GlobalZoomLevel https://visualstudiogallery.msdn.microsoft.com/59260ee8-5e2d-4ca7-8dc5-4efcf7b61a81/file/130525/2/GlobalZoomLevel.vsix
 Install-ChocolateyVsixPackage WebEssentials2013Update4 https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/44/WebEssentials2013.vsix
+Install-ChocolateyVsixPackage NUnitTestAdapater https://visualstudiogallery.msdn.microsoft.com/6ab922d0-21c0-4f06-ab5f-4ecd1fe7175d/file/66177/15/NUnitVisualStudioTestAdapter-1.2.vsix
 
 
 #Git
