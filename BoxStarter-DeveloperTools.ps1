0b12fdeb5357817c5e49b2992f7fa35cbdffe7fc
Joe Rall <Joseph.Rall@Live.com>
2015-05-06 11:20:47 -0400

    Added AddNewFile & VSCommands

--- BoxStarter-DeveloperTools.ps1
+++ BoxStarter-DeveloperTools.ps1
@@ -16,6 +16,8 @@ Install-ChocolateyVsixPackage GlobalZoomLevel https://visualstudiogallery.msdn.m
 Install-ChocolateyVsixPackage WebEssentials2013Update4 https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/44/WebEssentials2013.vsix
 Install-ChocolateyVsixPackage NUnitTestAdapater https://visualstudiogallery.msdn.microsoft.com/6ab922d0-21c0-4f06-ab5f-4ecd1fe7175d/file/66177/15/NUnitVisualStudioTestAdapter-1.2.vsix
 Install-ChocolateyVsixPackage OpenInExternalBrowser https://visualstudiogallery.msdn.microsoft.com/46c0c49e-f825-454b-9f6a-48b216797eb5/file/136677/1/Tvl.VisualStudio.OpenInExternalBrowser.vsix
+Install-ChocolateyVsixPackage AddNewFile https://visualstudiogallery.msdn.microsoft.com/3f820e99-6c0d-41db-aa74-a18d9623b1f3/file/140782/17/Add%20New%20File%20v2.1.39.vsix
+Install-ChocolateyVsixPackage VSCommands https://visualstudiogallery.msdn.microsoft.com/c6d1c265-7007-405c-a68b-5606af238ece/file/106247/21/SquaredInfinity.VSCommands.VS12.vsix
 
 
 #Git
